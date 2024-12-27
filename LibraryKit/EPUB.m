3c33a2811765ab5bdf4d704ab69abd447c4e848a
Don McCaughey <don@donm.cc>
2024-12-26 22:41:00 -0600

    Added minizip-ng 4.0.7 for reading EPUB files.
    Vendored the source distribution of [minizip-ng][1] into the `libs/`
    directory and added a Framework target for it.  Also created umbrella
    header `minizip.h` for the framework.
    
    Extended the `EPUB` class init method to open the EPUB zip file and look
    for the `container.xml` file in the manifest and as a fall-back, find
    any `.opf` file.
    
    [1]: https://github.com/zlib-ng/minizip-ng

--- LibraryKit/EPUB.m
+++ LibraryKit/EPUB.m
@@ -1,5 +1,11 @@
 #import "EPUB.h"
 
+#import <minizip/minizip.h>
+
+
+BOOL
+endsWith(char const *s, char const *suffix);
+
 
 @implementation EPUB
 
@@ -19,8 +25,50 @@
     _pageCount = 0;
     _title = nil;
     
+    int32_t error;
+    void *zipReader = mz_zip_reader_create();
+    error = mz_zip_reader_open_file(zipReader, path.UTF8String);
+    if (error) {
+        NSLog(@"Failed to open EPUB at '%@'", path);
+        mz_zip_reader_delete(&zipReader);
+        return self;
+    }
+    
+    mz_zip_file *zipFile;
+    error = mz_zip_reader_goto_first_entry(zipReader);
+    while ( ! error) {
+        error = mz_zip_reader_entry_get_info(zipReader, &zipFile);
+        if (error) {
+            NSLog(@"Failed to read entry from EPUB at '%@'", path);
+        } else {
+            if (0 == strcasecmp("META-INF/container.xml", zipFile->filename)) {
+                NSLog(@"Found 'container.xml' in EPUB '%@'", path.lastPathComponent);
+                // TODO: parse container.xml to find root files with mimetype application/oebps-package+xml
+                // TODO: fall back to searching for a .opf file if this fails
+            } else if (endsWith(zipFile->filename, ".opf")) {
+                NSLog(@"Found '%s' in EPUB '%@'", zipFile->filename, path.lastPathComponent);
+            }
+            error = mz_zip_reader_goto_next_entry(zipReader);
+        }
+    }
+    if (MZ_END_OF_LIST != error) {
+        NSLog(@"Error reading entries from EPUB at '%@': %d", path, error);
+    }
+    
+    mz_zip_reader_close(zipReader);
+    mz_zip_reader_delete(&zipReader);
+    
     return self;
 }
 
 
 @end
+
+
+BOOL
+endsWith(char const *s, char const *suffix)
+{
+    size_t suffix_len = strlen(suffix);
+    char const *suffix_start = strstr(s, suffix);
+    return suffix_start && ! suffix_start[suffix_len];
+}
---
+++ libs/minizip/minizip.h
@@ -0,0 +1,21 @@
+#import <Foundation/Foundation.h>
+
+FOUNDATION_EXPORT double minizipVersionNumber;
+
+FOUNDATION_EXPORT const unsigned char minizipVersionString[];
+
+
+#import <minizip/mz.h>
+#import <minizip/mz_crypt.h>
+#import <minizip/mz_os.h>
+#import <minizip/mz_strm.h>
+#import <minizip/mz_strm_buf.h>
+#import <minizip/mz_strm_bzip.h>
+#import <minizip/mz_strm_libcomp.h>
+#import <minizip/mz_strm_mem.h>
+#import <minizip/mz_strm_os.h>
+#import <minizip/mz_strm_pkcrypt.h>
+#import <minizip/mz_strm_split.h>
+#import <minizip/mz_strm_wzaes.h>
+#import <minizip/mz_zip.h>
+#import <minizip/mz_zip_rw.h>
